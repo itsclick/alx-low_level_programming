d7808c4dd81f54fc143b2f1e3d1a5017f0e5c785
itsclick <sammyglobalworld@gmail.com>
2022-11-07 07:14:01 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,23 +1,48 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
+ * *str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to new space in memory or null
+ **/
+char *str_concat(char *s1, char *s2)
 {
-	char *s;
+	int i, j, k, e, coun;
+	int coun1 = 0;
+	int coun2 = 0;
+	char *q;
 
-	s = str_concat("Betty ", "Holberton");
-	if (s == NULL)
+	if (s1 == NULL)
 	{
-		printf("failed\n");
-		return (1);
+		s1 = "";
 	}
-	printf("%s\n", s);
-	free(s);
-	return (0);
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		coun1 = coun1 + 1;
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		coun2 = coun2 + 1;
+	}
+	coun = coun1 + coun2;
+	q = malloc(sizeof(char) * coun + 1);
+	if (q == NULL)
+	{
+		return (NULL);
+	}
+	for (k = 0; s1[k] != '\0'; k++)
+	{
+		q[k] = s1[k];
+	}
+	for (k = k, e = 0; s2[e] != '\0'; e++, k++)
+	{
+		q[k] = s2[e];
+	}
+	q[k] = '\0';
+	return (q);
 }
